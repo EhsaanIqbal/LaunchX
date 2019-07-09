e47267735f1cb6f358bbd0ee682883955231ba7e
Ehsaan Iqbal <ishu283@gmail.com>
2019-07-09 16:39:39 +0400

    refined ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import logo from "./launchx.png";
-import Home from './components/Home';
-import Mission from './components/Mission';
+import Home from "./components/Home";
+import Mission from "./components/Mission";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -15,15 +15,15 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-      <div className="container">
-        <img
-          src={logo}
-          alt="LaunchX"
-          style={{ width: 300, display: "block", margin: "auto" }}
-        />
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/launch/:flight_number" component={Mission}/>
-      </div>
+        <div className="container">
+          <img
+            src={logo}
+            alt="LaunchX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/launch/:flight_number" component={Mission} />
+        </div>
       </Router>
     </ApolloProvider>
   );
